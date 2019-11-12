fb7dccf55739c456a7f49427f1d63bd74e25f4c0
Mo5hu <k173634@nu.edu.pk>
2019-11-12 15:03:01 +0500

    Login and Register page added; Firebase registeration step 6

---
+++ src/app/firebase.ts
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCKaugLVz9QYipqt7L5M8DrPeuzSm5wHAI",
+    authDomain: "social-media-app-1b3c0.firebaseapp.com",
+    databaseURL: "https://social-media-app-1b3c0.firebaseio.com",
+    projectId: "social-media-app-1b3c0",
+    storageBucket: "social-media-app-1b3c0.appspot.com",
+    messagingSenderId: "437519794650",
+    appId: "1:437519794650:web:80146185b5df6857a980e3",
+    measurementId: "G-C09WF729Z0"
+  }
+
+  export default firebaseConfig
